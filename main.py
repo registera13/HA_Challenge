c0ba9a47e31d19ef7799f8be795dade021726ece
alanr <registera13@gmail.com>
2020-03-08 13:09:54 -0500

    gitignores

--- main.py
+++ main.py
@@ -25,14 +25,23 @@ class Hg19(object):
 
     def sort_gene(self,ref):
 
-        pass
+        if 2 and 3 in range(3, 9):
+            print(" %s is in the range")
+        else:
+            print("The number is outside the given range.")
+        return
 
-    def sort_exon(self,ref):
-        pass
+
+    def sort_exon(self,df):
+        for index, row in df.iterrows():
+
+            if row[1] and row[2] in range():
 
 
 if __name__ == "__main__":
-    hr19 = Hg19()
-    hr19.read_data()
+    data = pd.read_csv('Data Files/hg5_min_6_min_7.bed', sep='\t', header=None)
+    test=Hg19()
+    test.sort_exon(data)
+
 
 
